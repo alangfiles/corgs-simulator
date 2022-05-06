212c558cac4e72bd269545ba544a97b063d65299
Alan Files <alangfiles@users.noreply.github.com>
2022-05-06 00:11:22 -0400

    girlsprites added

---
+++ girlsprites.c
@@ -0,0 +1,129 @@
+#pragma region playersprites
+
+const unsigned char PrizeGuy94[] = {
+
+		0, 0, 0x26, 2,
+		8, 0, 0x27, 2,
+		0, 8, 0x36, 2,
+		8, 8, 0x37, 2,
+		128
+
+};
+
+const unsigned char PlayerSprDown[] = {
+		0, 0, 0x42, 2,
+		8, 0, 0x43, 2,
+		0, 8, 0x52, 2,
+		8, 8, 0x52, 2 | OAM_FLIP_H,
+		128
+
+};
+
+const unsigned char PlayerSprDownTwo[] = {
+		0, 0, 0x42, 2,
+		8, 0, 0x43, 2,
+		0, 8, 0x52, 2,
+		8, 8, 0x53, 2,
+		128};
+
+const unsigned char PlayerSprDownThree[] = {
+		0, 0, 0x42, 2,
+		8, 0, 0x43, 2,
+		0, 8, 0x53, 2 | OAM_FLIP_H,
+		8, 8, 0x52, 2 | OAM_FLIP_H,
+		128};
+
+const unsigned char PlayerSprUp[] = {
+		0, 0, 0x44, 2,
+		8, 0, 0x45, 2,
+		0, 8, 0x54, 2,
+		8, 8, 0x54, 2 | OAM_FLIP_H,
+		128};
+
+const unsigned char PlayerSprUpTwo[] = {
+		0, 0, 0x44, 2,
+		8, 0, 0x45, 2,
+		0, 8, 0x54, 2,
+		8, 8, 0x55, 2,
+		128
+
+};
+
+const unsigned char PlayerSprUpThree[] = {
+		0, 0, 0x44, 2,
+		8, 0, 0x45, 2,
+		0, 8, 0x55, 2 | OAM_FLIP_H,
+		8, 8, 0x54, 2 | OAM_FLIP_H,
+		128};
+
+const unsigned char PlayerSprRight[] = {
+		0, 0, 0x40, 2,
+		8, 0, 0x41, 2,
+		0, 8, 0x50, 2,
+		8, 8, 0x51, 2,
+		128};
+
+const unsigned char PlayerSprRightTwo[] = {
+		0, 0, 0x40, 2,
+		8, 0, 0x41, 2,
+		0, 8, 0x56, 2,
+		8, 8, 0x57, 2,
+		128};
+
+const unsigned char PlayerSprRightThree[] = {
+		0, 0, 0x40, 2,
+		8, 0, 0x41, 2,
+		0, 8, 0x46, 2,
+		8, 8, 0x47, 2,
+		128};
+
+const unsigned char PlayerSprRightFour[] = {
+			0, 0, 0x40, 2,
+		8, 0, 0x41, 2,
+		0, 8, 0x56, 2,
+		8, 8, 0x57, 2,
+		128};
+
+const unsigned char PlayerSprRightFive[] = {
+		0, 0, 0x40, 2,
+		8, 0, 0x41, 2,
+		0, 8, 0x46, 2,
+		8, 8, 0x47, 2,
+		128};
+
+const unsigned char PlayerSprLeft[] = {
+		0, 0, 0x41, 2 | OAM_FLIP_H,
+		8, 0, 0x40, 2 | OAM_FLIP_H,
+		0, 8, 0x51, 2 | OAM_FLIP_H,
+		8, 8, 0x50, 2 | OAM_FLIP_H,
+		128};
+
+const unsigned char PlayerSprLeftTwo[] = {
+		0, 0, 0x41, 2 | OAM_FLIP_H,
+		8, 0, 0x40, 2 | OAM_FLIP_H,
+		0, 8, 0x57, 2 | OAM_FLIP_H,
+		8, 8, 0x56, 2 | OAM_FLIP_H,
+		128};
+
+const unsigned char PlayerSprLeftThree[] = {
+		0, 0, 0x41, 2| OAM_FLIP_H,
+		8, 0, 0x40, 2 | OAM_FLIP_H,
+		0, 8, 0x47, 2 | OAM_FLIP_H,
+		8, 8, 0x46, 2 | OAM_FLIP_H,
+		128};
+
+const unsigned char PlayerSprLeftFour[] = {
+		0, 0, 0x41, 2 | OAM_FLIP_H,
+		8, 0, 0x40, 2 | OAM_FLIP_H,
+		0, 8, 0x57, 2 | OAM_FLIP_H,
+		8, 8, 0x56, 2 | OAM_FLIP_H,
+		128};
+
+const unsigned char PlayerSprLeftFive[] = {
+		0, 0, 0x41, 2| OAM_FLIP_H,
+		8, 0, 0x40, 2 | OAM_FLIP_H,
+		0, 8, 0x47, 2 | OAM_FLIP_H,
+		8, 8, 0x46, 2 | OAM_FLIP_H,
+		128};
+
+#pragma endregion playersprites
